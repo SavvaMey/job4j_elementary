064531b552c23a60d8460a94d5b2bb00a3cdbed9
SavvaMey <savva28031997@gmail.com>
2020-08-09 22:57:40 +0300

    9.1. String [#340308]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -5,7 +5,7 @@ public class ArgMethod {
         System.out.println("Hello, " + name);
     }
 
-  //внесенные изменения:
+  // внесенные изменения:
 
     @SuppressWarnings("checkstyle:WhitespaceAround")
     public static void main(String[] args) {
