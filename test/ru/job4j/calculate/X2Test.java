8bac3a2134d6bce3c55240df4543629be9acd151
SavvaMey <savva28031997@gmail.com>
2020-08-10 18:22:40 +0300

    1. Модульные тесты. [#340337]

---
+++ test/ru/job4j/calculate/X2Test.java
@@ -0,0 +1,73 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class X2Test {
+
+    @Test
+    public void whenAll1then3() {
+        /* Входные параметры. Их будет 4 штуки */
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        /* ожидаемое значение. Это всегда одна переменная. */
+        int expected = 3;
+        /* вызов метода, который хотим проверить. */
+        /* Результат вычисления. Это всегда одна переменная. */
+        int rsl = X2.calc(a, b, c, x);
+        /* сравнение полученного значения с ожидаемым */
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenA0then2() {
+        /* Входные параметры. Их будет 4 штуки */
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        /* ожидаемое значение. Это всегда одна переменная. */
+        int expected = 2;
+        /* вызов метода, который хотим проверить. */
+        /* Результат вычисления. Это всегда одна переменная. */
+        int rsl = X2.calc(a, b, c, x);
+        /* сравнение полученного значения с ожидаемым */
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenC0then2() {
+        /* Входные параметры. Их будет 4 штуки */
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        /* ожидаемое значение. Это всегда одна переменная. */
+        int expected = 2;
+        /* вызов метода, который хотим проверить. */
+        /* Результат вычисления. Это всегда одна переменная. */
+        int rsl = X2.calc(a, b, c, x);
+        /* сравнение полученного значения с ожидаемым */
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenX0then1() {
+        /* Входные параметры. Их будет 4 штуки */
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        /* ожидаемое значение. Это всегда одна переменная. */
+        int expected = 1;
+        /* вызов метода, который хотим проверить. */
+        /* Результат вычисления. Это всегда одна переменная. */
+        int rsl = X2.calc(a, b, c, x);
+        /* сравнение полученного значения с ожидаемым */
+        assertThat(rsl, is(expected));
+    }
+}
