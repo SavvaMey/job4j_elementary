e6242aa7ef863c9d7e36d3f7d1d8b0eefa4929b9
SavvaMey <savva28031997@gmail.com>
2020-08-10 18:46:55 +0300

    1.1. Входные данные в тесте [#340342]

---
+++ test/ru/job4j/fit/FitTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.fit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class FitTest {
+
+    @Test
+    public void ThenManHeightt187When100() {
+        short height = 187;
+        double expected = 100.05;
+        double out = Fit.manWeight(height);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void ThenWomanHeight190When92() {
+        short height = 190;
+        double expected = 92;
+        double out = Fit.womanWeight(height);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
