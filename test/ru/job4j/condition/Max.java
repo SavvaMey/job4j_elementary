e53c385fe14d0f310f37bec0b9127d3b76faae7b
SavvaMey <savva28031997@gmail.com>
2020-08-10 22:44:17 +0300

    4.2. Тернарное сравнение [#340260]

---
+++ test/ru/job4j/condition/Max.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int rsl = (left > right) ? left : right;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(5,2));
+        System.out.println(max(2,5));
+    }
+}
