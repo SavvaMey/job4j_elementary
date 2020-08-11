bc39432602bdb767376ef06d6305623c7206777c
SavvaMey <savva28031997@gmail.com>
2020-08-11 23:47:33 +0300

    5.9. Крест в псевдографике. [#340357]

---
+++ src/ru/job4j/loop/Slash.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                if (cell == row || cell + row + 1 == size) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+        System.out.println("Draw by 7");
+        draw(7);
+    }
+}
