a5349a1aed3136c1fddf894b33259f120fea5a5d
SavvaMey <savva28031997@gmail.com>
2020-08-12 00:19:31 +0300

    6.0.2. Заполнение массива. [#340280]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
+    // old
     short[] ages = new short[10];
     String[] surnames = new String[100500];
     float[] prices = new float[40];
