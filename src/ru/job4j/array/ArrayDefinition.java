2e6dacd07f78167847e9ff994cd98e535760d9be
SavvaMey <savva28031997@gmail.com>
2020-08-12 00:18:58 +0300

    6.0.2. Заполнение массива. [#340280]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,4 +4,17 @@ public class ArrayDefinition {
     short[] ages = new short[10];
     String[] surnames = new String[100500];
     float[] prices = new float[40];
+
+    public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "Savva";
+        names[1] = "Petr";
+        names[2] = "Niko";
+        names[3] = "Alex";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+    }
+
 }
