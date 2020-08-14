8ce8fc54a8b2e5feb89d36558c6e4f97f00861bc
SavvaMey <savva28031997@gmail.com>
2020-08-14 23:40:24 +0300

    1. Чтение кода и базовые ошибки [#340453]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -9,10 +9,8 @@ public class Point {
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
-
         result = Point.distance(1, 5, 2, 8);
         System.out.println("result (1, 5) to (2, 8) " + result);
-
         result = Point.distance(-1, 5, 0, -8);
         System.out.println("result (-1, 5) to (0, -8) " + result);
     }
