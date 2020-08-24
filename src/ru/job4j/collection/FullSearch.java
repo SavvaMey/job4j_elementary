788468bd5cc1a53325bffda6a8a4f8f45add56b6
SavvaMey <savva28031997@gmail.com>
2020-08-24 12:25:09 +0300

    1. Уникальные задачи. [#340397]

--- src/ru/job4j/collection/FullSearch.java
+++ src/ru/job4j/collection/FullSearch.java
@@ -2,7 +2,7 @@ package ru.job4j.collection;
 
 import java.util.HashSet;
 import java.util.List;
-
+//set
 public class FullSearch {
     public static HashSet<String> extractNumber(List<Task> list) {
         HashSet<String> numbers = new HashSet<>();
