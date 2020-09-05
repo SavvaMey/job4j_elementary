de252acaff2423cfeb36899158897383b3dda1f3
SavvaMey <savva28031997@gmail.com>
2020-09-05 07:16:38 +0300

    1. Организовать сортировку User [#340380]

--- test/ru/job4j/collection/UserTest.java
+++ test/ru/job4j/collection/UserTest.java
@@ -24,7 +24,6 @@ public class UserTest {
     @Test
     public void whenComparePertVSIvan() {
         int rsl = new User("Petr", 32).compareTo(new User("Ivan", 31));
-        System.out.println(rsl);
         assertThat(rsl, greaterThan(0));
     }
 
