94f020acb7c7d6658f7884a818ab40c76b3f0cd0
SavvaMey <savva28031997@gmail.com>
2020-09-07 22:46:29 +0300

    2. Отсортировать департаменты [#340374]

---
+++ src/ru/job4j/collection/Departments.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        List<String> rsl = new ArrayList<>();
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                start += el;
+                tmp.add(start);
+                start += "/";
+                // tmp.add start + "/" + el
+            }
+        }
+        return  new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        Collections.sort(orgs, new DepDescComp());
+    }
+}
---
+++ test/ru/job4j/collection/DepartmentsTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static ru.job4j.collection.Departments.sortAsc;
+import static ru.job4j.collection.Departments.sortDesc;
+
+public class DepartmentsTest {
+    @Test
+    public void whenMissed() {
+        List<String> input = Arrays.asList("k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNonChange() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNaturalSort() {
+        List<String> input = Arrays.asList("k2", "k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2");
+        sortAsc(input);
+        assertThat(input, is(expect));
+    }
+
+    @Test
+    public void whenReverseSort() {
+        List<String> input = Arrays.asList("k2/sk2", "k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/ssk1");
+        List<String> expect = Arrays.asList("k2/sk2", "k2/sk1/ssk1", "k2/sk1", "k2", "k1/sk1", "k1");
+        sortDesc(input);
+        assertThat(input, is(expect));
+    }
+
+    @Test
+    public void whenReverseSortAnother() {
+        List<String> input = Arrays.asList("K2/SK1/SSK2", "K2/SK1/SSK1");
+        List<String> expect = Arrays.asList("K2/SK1/SSK2", "K2/SK1/SSK1");
+        sortDesc(input);
+        assertThat(input, is(expect));
+    }
+
+    @Test
+    public void whenReverseSortAnotherTwo() {
+        List<String> input = Arrays.asList("K2", "K2/SK1");
+        List<String> expect = Arrays.asList("K2/SK1", "K2");
+        sortDesc(input);
+        assertThat(input, is(expect));
+    }
+
+}
