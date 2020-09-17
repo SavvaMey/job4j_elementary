c80a3a365ad54f67ec5f8b238352e373d473264b
SavvaMey <savva28031997@gmail.com>
2020-09-17 16:49:00 +0300

    6. Функции высшего порядка [#340208]

--- src/ru/job4j/search/PhoneDictionary.java
+++ src/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -9,14 +10,28 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
-    public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person person : persons) {
-            if (person.getAddress().contains(key) || person.getName().contains(key) || person.getSurname().contains(key)
-                    || person.getPhone().contains(key)) {
-                result.add(person);
-            }
+//    public ArrayList<Person> find(String key) {
+//        ArrayList<Person> result = new ArrayList<>();
+//        for (Person person : persons) {
+//            if (person.getAddress().contains(key) || person.getName().contains(key) || person.getSurname().contains(key)
+//                    || person.getPhone().contains(key)) {
+//                result.add(person);
+//            }
+//        }
+//        return result;
+//    }
+public ArrayList<Person> find(String key) {
+    Predicate<Person> checkName = (person) -> person.getName().equals(key);
+    Predicate<Person> checkSurname = (person) -> person.getSurname().equals(key);
+    Predicate<Person> checkPhone = (person) -> person.getPhone().equals(key);
+    Predicate<Person> checkAdr = (person) -> person.getAddress().equals(key);
+    Predicate<Person> combine = checkName.or(checkSurname.or(checkPhone).or(checkAdr));
+    ArrayList<Person> result = new ArrayList<>();
+    for (Person person : persons) {
+        if (combine.test(person)) {
+            result.add(person);
         }
-        return result;
     }
+    return result;
+}
 }
--- test/ru/job4j/search/PhoneDictionaryTest.java
+++ test/ru/job4j/search/PhoneDictionaryTest.java
@@ -6,13 +6,13 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
-
-    @Test
+   @Test
     public void find() {
         PhoneDictionary phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev",  "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        ArrayList<Person> persons = phones.find("534872");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
-    } }
+    }
+     }
