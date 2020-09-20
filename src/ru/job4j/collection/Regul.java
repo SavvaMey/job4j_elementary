1d36e80f4df1c4f88c0ce331aab8465412bf2168
SavvaMey <savva28031997@gmail.com>
2020-09-20 18:02:27 +0300

    123

---
+++ src/ru/job4j/collection/Regul.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+public class Regul {
+    /*
+    \\d - одна цифра
+    \\w - одна буква
+    + - 1 или более
+    * - 0 или более
+    ? - 0 или 1 символов до
+    (x|y|z) - 1 из 3
+    [abc]=(a|b|с)
+    [a-zA-Z] - все английские буквы
+    [0-9]-\\d
+    [^abc] - любой символ кроме a,b,c
+    . - любой символ
+    {2} - 2 символа по (\\d{2})
+    {2,} - 2 или более символа (\\d{2,})
+    {2,4} - от 2 до 4 символов
+    String. replaceAll - пригимает regex
+     String. replaceFirst - пригимает regex
+     String. replace - просто строки
+     pattern matcher
+     Pattern.compile()
+     mathcer find group
+   то что в ркуглой скобке группа - имеет свой айди group(id)
+     */
+
+
+}
